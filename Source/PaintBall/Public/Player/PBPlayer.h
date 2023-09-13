365d4823b495c6ae54b7347eae6f24ddf4c30791
Cocaine <kokotkin.alexander@gmail.com>
2023-09-14 02:42:28 +0300

    Separate player functionality

---
+++ Source/PaintBall/Public/Player/PBPlayer.h
@@ -0,0 +1,42 @@
+// // PaintBall. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/PBBasePawn.h"
+#include "PBPlayer.generated.h"
+
+class USphereComponent;
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class PAINTBALL_API APBPlayer : public APBBasePawn
+{
+    GENERATED_BODY()
+    
+public:
+    APBPlayer(const FObjectInitializer& ObjInit);
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    USphereComponent* SphereComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    USpringArmComponent* SpringArmComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UCameraComponent* CameraComponent;
+
+public:
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+    UFUNCTION()
+    void OnCollision(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Hit);
+
+private:
+    void MoveForward(float Value);
+    void MoveRight(float Value);
+    void LookAround(float Value);
+};
