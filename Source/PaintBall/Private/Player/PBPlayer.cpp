5ac468e1be6098be2c062cd0b7d02702bec49355
Cocaine <kokotkin.alexander@gmail.com>
2023-10-15 18:56:00 +0300

    Add force direction arrow
    Arrow change size from force value
    Arrow show Player move direction

--- Source/PaintBall/Private/Player/PBPlayer.cpp
+++ Source/PaintBall/Private/Player/PBPlayer.cpp
@@ -6,13 +6,15 @@
 #include "AI/PBAICleaner.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/ArrowComponent.h"
 #include "GameFramework/Controller.h"
 #include "Components/SphereComponent.h"
 #include "Components/PBPaintComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "Sound/SoundCue.h"
 
-APBPlayer::APBPlayer(const FObjectInitializer& ObjInit) : Super(ObjInit)
+APBPlayer::APBPlayer(const FObjectInitializer& ObjInit)
+    : Super(ObjInit)
 {
     SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
     SphereComponent->SetupAttachment(StaticMeshComponent);
@@ -22,7 +24,7 @@ APBPlayer::APBPlayer(const FObjectInitializer& ObjInit) : Super(ObjInit)
     bUseControllerRotationPitch = false;
     bUseControllerRotationYaw = false;
     bUseControllerRotationRoll = false;
-    
+
     SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 
     SpringArmComponent->bUsePawnControlRotation = true;
@@ -31,7 +33,11 @@ APBPlayer::APBPlayer(const FObjectInitializer& ObjInit) : Super(ObjInit)
     SpringArmComponent->SetupAttachment(StaticMeshComponent);
 
     CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-    CameraComponent->SetupAttachment(SpringArmComponent,  USpringArmComponent::SocketName);
+    CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+
+    ArrowComponent = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponent"));
+    ArrowComponent->SetupAttachment(CameraComponent);
+    ArrowComponent->SetHiddenInGame(false);
 }
 
 void APBPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -54,23 +60,36 @@ void APBPlayer::OnCollision(UPrimitiveComponent* OverlappedComponent, AActor* Ot
     {
         UGameplayStatics::SpawnSoundAtLocation(GetWorld(), OnCollisionSound, GetActorLocation(), GetActorRotation(), SoundVolume);
     }
-    
+
     OtherPaintComponent = OtherActor->GetComponentByClass<UPBPaintComponent>();
 }
 
 void APBPlayer::MoveForward(float Value)
 {
-    if (CameraComponent && Value != 0.0f)
+    if (CameraComponent)
     {
-        StaticMeshComponent->AddForce(CameraComponent->GetForwardVector() * Value * Speed * StaticMeshComponent->GetMass());
+        StaticMeshComponent->AddForce(CameraComponent->GetForwardVector() * Value * Speed, TEXT("None"), true);
+
+        ArrowComponent->SetWorldRotation(
+            FRotationMatrix::MakeFromX(
+                StaticMeshComponent->GetPhysicsLinearVelocity().GetSafeNormal().RotateAngleAxis(90.0f, FVector::UpVector)).Rotator());
+        
+        ArrowComponent->SetArrowLength(FMath::Clamp(MinArrowSize,FMath::Abs(StaticMeshComponent->GetPhysicsLinearVelocity().X * ArrowSizeMod), MaxArrowSize));
+
     }
 }
 
 void APBPlayer::MoveRight(float Value)
 {
-    if (CameraComponent && Value != 0.0f)
+    if (CameraComponent)
     {
-        StaticMeshComponent->AddForce(CameraComponent->GetRightVector() * Value * Speed * StaticMeshComponent->GetMass());
+        StaticMeshComponent->AddForce(CameraComponent->GetRightVector() * Value * Speed, TEXT("None"), true);
+
+        ArrowComponent->SetWorldRotation(
+            FRotationMatrix::MakeFromX(StaticMeshComponent->GetPhysicsLinearVelocity().GetSafeNormal()).Rotator());
+
+        ArrowComponent->SetArrowLength(FMath::Clamp(MinArrowSize,FMath::Abs(StaticMeshComponent->GetPhysicsLinearVelocity().X * ArrowSizeMod), MaxArrowSize));
+
     }
 }
 
@@ -81,5 +100,6 @@ void APBPlayer::LookAround(float Value)
         // find out which way is right
         const FRotator Rotation = Controller->GetControlRotation();
         AddControllerYawInput(Value);
+
     }
 }
--- Source/PaintBall/Public/Player/PBPlayer.h
+++ Source/PaintBall/Public/Player/PBPlayer.h
@@ -10,6 +10,7 @@ class USphereComponent;
 class USpringArmComponent;
 class UCameraComponent;
 class USoundCue;
+class UArrowComponent;
 
 UCLASS()
 class PAINTBALL_API APBPlayer : public APBBasePawn
@@ -29,19 +30,32 @@ protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
     UCameraComponent* CameraComponent;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UArrowComponent* ArrowComponent;
+    
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Sound")
     USoundCue* OnCollisionSound;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Sound")
     float SoundVolume = 0.3f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Arrow")
+    float MinArrowSize = 30.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Arrow")
+    float MaxArrowSize = 50.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Arrow", meta = (ClampMin = "0.2", ClampMax = "0.8"))
+    float ArrowSizeMod = 0.5f;
     
 public:
     // Called to bind functionality to input
     virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
     
     virtual void OnCollision(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Hit) override;
-
+    
 private:
+    
     void MoveForward(float Value);
     void MoveRight(float Value);
     void LookAround(float Value);
